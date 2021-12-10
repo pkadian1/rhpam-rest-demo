9d360b106643b1763aa45099966776441c2e684b
adminUser <>
2021-12-10 10:59:27 +0000

    {/src/main/java/com/myspace/rest_project/test.java}

--- src/main/java/com/myspace/rest_project/test.java
+++ src/main/java/com/myspace/rest_project/test.java
@@ -6,12 +6,23 @@ package com.myspace.rest_project;
 
 public class test implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public test() {
-    }
+	private java.lang.String name;
 
+	public test() {
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
 
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public test(java.lang.String name) {
+		this.name = name;
+	}
 
 }
